8091b7003bf517337afcf04d6c31d590d2817979
Yauheni Khotsin <yauheni_khotsin@epam.com>
2020-05-27 12:16:07 +0300

    Add a deleting a post.

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(Context);
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
   
   return (
     <View>
@@ -21,12 +21,15 @@ const IndexScreen = () => {
               style={styles.row}>
               <Text
                 style={styles.title}>
-                {item.title}
+                {item.title} - {item.id}
               </Text>
-              <Feather
-                style={styles.icon}
-                name="trash"
-              />
+              <TouchableOpacity
+                onPress={() => deleteBlogPost(item.id)}>
+                <Feather
+                  style={styles.icon}
+                  name="trash"
+                />
+              </TouchableOpacity>
             </View>
           );
         }}
