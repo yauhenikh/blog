4a133a5bb3f4decb39eb92104b8ec1c47beac5c8
Yauheni Khotsin <yauheni_khotsin@epam.com>
2020-05-27 17:20:19 +0300

    Add header navigation.

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const CreateScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Create Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
