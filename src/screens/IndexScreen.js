834118a5753cd143e639cd152937737ded371f62
Yauheni Khotsin <yauheni_khotsin@epam.com>
2020-05-27 11:28:12 +0300

    Automate context creation.

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
-import BlogContext from '../context/BlogContext';
+import { Context } from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const { data, addBlogPost } = useContext(BlogContext);
+  const { state, addBlogPost } = useContext(Context);
   
   return (
     <View>
@@ -13,7 +13,7 @@ const IndexScreen = () => {
         onPress={addBlogPost}
       />
       <FlatList
-        data={data}
+        data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           return <Text>{item.title}</Text>
