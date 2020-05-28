390b2fb613782f77b9b97ea5f821e2aa96bc0d0d
Yauheni Khotsin <yauheni_khotsin@epam.com>
2020-05-28 13:29:01 +0300

    Add navigation on save.

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -29,7 +29,9 @@ const CreateScreen = ({ navigation }) => {
       />
       <Button
         title="Add Blog Post"
-        onPress={() => addBlogPost(title, content)}
+        onPress={() => addBlogPost(title, content, () => {
+          navigation.navigate('Index');
+        })}
       />
     </View>
   );
